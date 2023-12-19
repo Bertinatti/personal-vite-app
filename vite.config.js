e5450c9bee2d635d6d2d588f975a9241d58d4073
Bertinatti <leonardocardoso13@live.com>
2023-12-19 20:51:39 -0300

    changes for deploy gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/personal-vite-app",
+  base: "/personal-vite-app/",
   plugins: [react()],
 })
